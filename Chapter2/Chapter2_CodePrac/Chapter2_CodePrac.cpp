6f9c0ba8ff7fd3a81cf197577220497bd54ac31d
Chr11111s <2292312771@qq.com>
2025-10-13 16:45:40 +0800

    25-10-13

---
+++ Chapter2/Chapter2_CodePrac/Chapter2_CodePrac.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+/*
+Q1.编写程序,显示您的姓名与地址
+int main(){
+	cout << "姓名：Chris，地址：阿弥诺斯星" << endl;
+}
+*/
+
+
+/*
+Q2.编写程序，要求用户输入一个以long为单位的距离，然后将其转换为码(1 long = 220 码)
+int main() {
+	int dis, res;
+	std::cout << "输入距离(long): ";
+	std::cin >> dis;
+	res = dis * 220;
+	std::cout << "转换后距离为：" << res << "码";
+}
+*/
+
+
+//Q3.编写程序，它使用3个用户定义的函数(包括main()),并生成下面的输出：
+//Three blind mice
+//Three blind mice
+//See how they run
+//See how they run
+//其中一个函数要调用两次，该函数生成前两行；另一个函数也被调用两次，并生成其余的输出
+//
+//void first2lines(){
+//	std::cout<<"Three blind mice"<<std::endl;
+//	std::cout<<"Three blind mice"<<std::endl;
+//}
+//
+//void second2lines(){
+//	std::cout<<"See how they run"<<std::endl;
+//	std::cout<<"See how they run"<<std::endl;
+//}
+//
+//int main(){
+//	first2lines();
+//	second2lines();
+//}
+
+//Q4.编写程序，让用户输入其年龄，然后显示该年龄包含多少个月，例如29岁包含348个月
+//int main() {
+//	int age;
+//	std::cout << "Enter your age: ";
+//	std::cin >> age;
+//	std::cout << "Your age in months is: " << age * 12 << ".";
+//}
+
+//Q5.编写程序，其中main()调用一个用户定义的函数(以摄氏温度值为参数，并返回相应的华氏温度)。已知华氏温度 = 1.8 x 摄氏温度 + 32.0
+//double trans(double cel) {
+//	return cel * 1.8 + 32.0;
+//}
+//
+//int main() {
+//	double cel, fah;
+//	std::cout << "Please enter a Celsius value: ";
+//	std::cin >> cel;
+//	fah = trans(cel);
+//	std::cout << cel << " degrees Celsius is " << fah<<" degrees Fahrenheit.";
+//}
+
+//Q6.编写程序，其main()调用一个用户定义的函数(以光年值为参数，并返回对应天文单位的值)。已知1 光年 = 63240 天文单位
+//double trans(double ly) {
+//	return ly * 63240;
+//}
+//
+//int main() {
+//	double ly, au;
+//	std::cout << "Enter the number of light years: ";
+//	std::cin >> ly;
+//	au = trans(ly);
+//	std::cout << ly << " light years = " << au << " astronomical units.";
+//}
+
+//Q7.编写程序，要求用户输入小时数和分钟数。在main()中将这两个值传递给一个void函数。以小时数:分钟数显示时间
+//void show(int hour, int min) {
+//	std::cout <<"Time: "<< hour << ":" << min;
+//}
+//
+//int main() {
+//	int hour, min;
+//	std::cout << "Enter the number of hours: ";
+//	std::cin >> hour;
+//	std::cout << "Enter the number of minutes: ";
+//	std::cin >> min;
+//	show(hour, min);
+//}
---
+++ Chapter2/Chapter2_Review/Chapter2_Review.cpp
@@ -0,0 +1,39 @@
+﻿//Q1.C++程序的模块叫什么
+//A.函数，程序由一个 / 多个函数组成，main()是程序入口函数，其他函数用来把问题分解成更小模块。
+//
+//Q2.这行预处理器编译指令有什么用：#include <iostream>
+//A.这是一段预处理指令，把标准头文件iostream的内容在编译前包含进当前源文件，从而能让coder使用其内部声明的函数、类和对象。
+//
+//Q3.这行语句有什么用：using namespace std
+//A.将命名空间中的名字(标识符，包括函数、类、对象等)导入当前作用域。优点是方便书写，缺点易与自定义或其他库重名(自己起名vector, 而std::vector也存在)。
+//
+//Q4.如何打印"Hello, world"然后开始新的一行
+//A.cout << "Hello, world" << endl;
+//
+//Q5.如何创建名为cheeses的整数变量
+//A.int cheeses;
+//
+//Q6.如何将值32赋给变量cheeses
+//A.cheeses = 32;
+//
+//Q7.如何将从键盘输入的值读入变量cheeses
+//A.int cheeses;
+//  std::cin >> cheeses;
+//
+//Q8.如何打印"We have X vsrieties of cheese",其中X为变量cheeses当前值。
+//A.cout<<"We have"<cheeses<<"varieties of cheese";
+//
+//Q9.下面函数声明指出了关于函数的哪些信息
+//int froop(double t)-- - 返回值类型为int，传入一个形参(即调用时传入参数)t，类型为double类型。
+//void rattle(int n)-- - 无返回值，传入一个形参n，类型为int。
+//int prune(void)-- - 返回值类型为int，无传入参数。
+//
+//Q10.定义函数时，什么时候不必使用关键字return
+//A.①当返回类型为void时不强求使用，但如果需要提前返回也还是要用return的(比如递归)。②在写main函数到末尾时加不加return 0都无所谓，因为标准规定会隐式返回0
+//③非void型函数到末尾时必须要返回一个与声明一致的值，否则会报错(未定义行为)。
+//
+//Q11.假设你编写的main()函数包含以下代码
+//cout << "Please enter your PIN: ";
+//而编辑器指出cout是一个未知标识符。导致这种问题的原因可能是什么？给出3种解决方案
+//A.cout 属于 标准命名空间 std，报未知标识符可能是未包含头文件或者是未将名字引入当前作用域
+//①未包含头文件，检查是否#include <iostream>  ②未将名字引入当前作用域，修改代码为std::cout<<... ③使用using声明，using namespace std或者using std::cout
