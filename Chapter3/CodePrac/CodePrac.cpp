9f7004757e1f61d7e0d52ff353879a0cd9928125
Chr11111s <2292312771@qq.com>
2025-10-14 11:52:18 +0800

    25-10-14

---
+++ Chapter3/CodePrac/CodePrac.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+
+//Q1.用户输入一个整数指出自己身高(英寸)，然后转换为英尺和英寸。使用const表示转换因子
+//int main() {
+//	const int OneFeet_OneInch = 12; //1英尺 = 12英寸
+//	int height;
+//	std::cout << "输入身高：";
+//	std::cin >> height;  //byd的cout和cin一起整才能凑出个python的input()效果
+//
+//	int feet = height / OneFeet_OneInch;
+//	int restInch = height % OneFeet_OneInch;
+//	
+//	std::cout << "身高为 " << feet << "英尺" << restInch << "英寸" << std::endl;
+//}
+
+//Q2.输入身高(英尺英寸)，输入体重(磅)，使用三个变量储存。计算BMI指数：体重(千克)除以身高(米)的平方，1英尺 = 12英寸、1英寸 = 0.0254米、1千克 = 2.2磅。
+//int main() {
+//	int ft, in, ib; //ft-英尺，in-英寸，ib-磅
+//
+//	std::cout << "输入身高(英尺 + 英寸): ";
+//	std::cin >> ft >> in;
+//	std::cout << "输入体重(磅): ";
+//	std::cin >> ib;
+//
+//	int totalInch = ft * 12 + in;
+//	double meters = totalInch * 0.0254;
+//	double kg = ib / 2.2;
+//
+//	double BMI = kg / (meters * meters);
+//	std::cout << "BMI指数为：" << BMI;
+//}
+
+//Q3.以整数方式输入度、分、秒表示一个纬度，然后以度为单位显示该纬度。1度 = 60分，1分 = 60秒，以常量符号表示这些值。用三个变量储存输入值。
+//int main() {
+//	const int MINUTES_IN_DEGREE = 60;
+//	const int SECONDS_IN_MINUTE = 60;
+//
+//	int deg, min, sec;
+//	std::cout << "Enter a latitude in degrees, minutes, and seconds:\n";
+//	std::cout << "First, enter the degrees: ";
+//	std::cin >> deg;
+//	std::cout << "Next, enter the minutes of arc: ";
+//	std::cin >> min;
+//	std::cout << "Finallly, enter the seconds of arc: ";
+//	std::cin >> sec;
+//
+//	double res = deg + min / 60.0 + sec / 360.0;
+//	std::cout << deg << " degrees, " << min << " minutes, " << sec << " seconds = " << res << " degrees";
+//}
+
+//Q4.以整数形式读入秒数，用long或long long存储。然后以天、小时、分钟、秒的方式显示时间。使用常量符号表示换算关系。
+//int main() {
+//	const int HOURS_IN_DAY = 24;
+//	const int MINUTES_IN_HOUR = 60;
+//	const int SECONDS_IN_MINUTE = 60;
+//
+//	long long total_secs;
+//	std::cout << "Enter the number of seconds: ";
+//	std::cin >> total_secs;
+//	int days = total_secs / (HOURS_IN_DAY * MINUTES_IN_HOUR * SECONDS_IN_MINUTE);
+//	int rest_sec1 = total_secs % (HOURS_IN_DAY * MINUTES_IN_HOUR * SECONDS_IN_MINUTE);
+//
+//	int hours = rest_sec1 / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE);
+//	int rest_sec2 = rest_sec1 % (MINUTES_IN_HOUR * SECONDS_IN_MINUTE);
+//
+//	int mins = rest_sec2 / SECONDS_IN_MINUTE;
+//	int rest_sec3 = rest_sec2 % SECONDS_IN_MINUTE;
+//
+//	std::cout << total_secs << " seconds = " << days << " days, " << hours << " hours, " << mins << " minutes, " << rest_sec3 << " seconds";
+//}
+
+//Q5.读入全球总人口和美国当前人口，用long long存储，显示美国人口占全球人口。
+//int main() {
+//	long long global, us;
+//	std::cout << "Enter the world's population: ";
+//	std::cin >> global;
+//	std::cout << "Enter the population of the US: ";
+//	std::cin >> us;
+//
+//	double pers = (static_cast<double>(us) / static_cast<double>(global)) * 100;
+//	std::cout << "The population of the US is " << pers << "% of the world population";
+//}
+
+//Q6.读入驱车里程(英里)和使用汽油量(加仑)，计算每加仑够行驶多少英里，即 多少英里/加仑。
+//int main() {
+//	double mi, gas;
+//	std::cout << "提供总英里与总使用量：";
+//	std::cin >> mi >> gas;
+//	std::cout << (mi / gas) << "英里/加仑";
+//}
+
+//Q7.读入欧洲风格汽车耗油量(多少升/100公里)，然后转换为美国风格(多少英里/加仑)。100公里 = 62.14英里，1加仑 = 3.785升。
+int main() {
+	double gasL;
+	std::cout << "输入每100公里损耗油量(升): ";
+	std::cin >> gasL;
+
+	double gasG = gasL / 3.785; //现在是 多少加仑/62.14英里
+	double res = 62.14 / gasG;  //除以加仑数获取 1加仑/多少英里
+	std::cout << res << " 英里/加仑";
+}
---
+++ Chapter3/Review/Review.cpp
@@ -0,0 +1,33 @@
+﻿//Q1.为什么C++有多种整型？
+//A.为了适应不同的存储需求和计算需求，同时优化内存使用和性能。
+//
+//Q2.声明与下述描述相符的变量
+//a.short 整数，值为80---short可以声明较小范围的整数
+//b.unsigned int 整数，值为42110---unsigned int为无符号整数，可声明所有>=0的整数(较大的)
+//c.值为3000000000的整数---这种超过22亿过大的数用long long定义比较合适
+//
+//Q3.C++提供了什么措施来防止超出整型的范围
+//A.①对于无符号整型(unsigned int)溢出时规定发生环绕，即归零 ②提供多种整型类型(如int/unsigned int等)合理选用防止超出 
+//  ③C++11引入的固定宽度整型(如std::int32_t和std::int64_t)可以显式选择具体位数的整数，从而避免溢出
+//  ④可以手动设置std::numeric_limits的边界检查
+//
+//Q4.33L和33之间有什么区别？
+//A.数据类型不同，33L是long类型，33是int类型。而且在64位系统33L可以存储更大的数字。
+//
+//Q5.char grade = 65 与 char grade = 'A'是否等价
+//A.ASCII码值一致，都是65；但语法上不一样，65是整数，'A'是字符字面量。
+//
+//Q6.使用至少两种方法找出编码88表示的字符
+//A.①通过static_cast<char>(88)显示转换类型 ②强制类型转换cout<<(char)88
+//
+//Q7.将long值赋给float变量会导致舍入误差，将long值赋给double变量呢？将long long值赋给double变量呢？
+//A.有效数字位大小关系为float < long < long long < double , 所以long赋给float超出的部分会被舍弃，而long小于double所以赋值不会超出，不会导致舍入误差；
+//同理long long 赋给double也不会，除非赋的值过大，所以通常情况下不会出现舍入误差。
+//
+//Q8.8 * 9 + 2 = 19; 6 * 3 / 4 = 4(/ 在整数之间计算取整); 3 / 4 * 6 = 0; 6.0 * 3 / 4 = 4.5(/ 在存在浮点数计算保留小数); 15 % 4 = 1(%取余数，且%仅适用于整数，存在浮点会报错)
+//
+//Q9.假设x1,x2是两个double变量，如何将它们作为整数相加再赋给整型变量？将它们作为double相加再转换为int呢？
+//A.static_cast<int>(x1) + static_cast<int>(x2)即可；作为double相加则是static_cast<int>(x1 + x2)即可。static_cast<type>(变量名)具有强类型检查，编译时会检查转换是否合法，所以相较于直接加括号()不检查是否合法的转换更安全。
+//
+//Q10.指出声明变量类型：①auto cars = 15 (int) ②auto iou = 150.37_ (后缀_会被C++忽略，所以还是double) ③auto level = 'B' (char) ④auto crat = U'\U00002155' (char32_t，有8位数字故8 * 4，还有char16_t和char8_t，以此类推16有4个,而8一个也没有) 
+//⑤auto fract = 8.25f / 2.5 (double，C++默认小数精度为double,而分子为float，然而存在double结果就会是double)
