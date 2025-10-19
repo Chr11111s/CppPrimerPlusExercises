192e48d5d228e3e8ce636095c2b55b7e3b4ad059
Chr11111s <2292312771@qq.com>
2025-10-19 21:50:39 +0800

    25-10-19

---
+++ Chapter7/CodePrac/CodePrac.cpp
@@ -0,0 +1,280 @@
+﻿#include <iostream>
+
+//Q1.不断要求用户输入两个数，直到其中的一个为0。对于每两个数，程序将使用一个函数来计算它们的调和平均数，并将结果返回给main()，而后者报告结果。调和平均数=2.0 * x * y / ( x + y )
+//double cal(double x, double y) {
+//	return 2.0 * x * y / (x + y);
+//}
+//
+//int main() {
+//	int x, y;
+//	std::cout << "输入两数：";
+//	while (std::cin>>x>>y) {
+//		if (x == 0 || y == 0)break;
+//
+//		else {
+//			std::cout << "调和平均数：" << cal(x, y) << std::endl;
+//		}
+//		std::cout << "输入两数："; //找不准合理的提示位置？其实可以忽略其他语句只看cout，我们希望出现顺序为：输入两数——显示调和平均数——再次提示，那就按这样去排cout
+//	}
+//}
+
+////Q2.编写程序，要求用户输入最多10个高尔夫成绩，并将其存储在一个数组中。程序允许用户提早结束输入，并在一行上显示所有成绩，然后报告平均成绩。请使用3个数组处理函数来分别进行输入、显示和计算平均成绩。
+//int input(double arr[]) {
+//	double score;
+//	int count = 0;
+//	while (count < 10) {
+//		std::cin >> score;
+//		if (score < 0)break;
+//		arr[count] = score;
+//		count++;
+//	}
+//	return count;
+//}
+//
+//void show(double arr[], int count) {
+//	for (int i = 0; i < count; i++) {
+//		std::cout << arr[i] << " ";
+//	}
+//	std::cout << std::endl;
+//}
+//
+//double get_avg(double arr[], int count) {
+//	double avg;
+//	double total = 0;
+//	for (int i = 0; i < count; i++) {
+//		total += arr[i];
+//	}
+//	avg = total / count;
+//	return avg;
+//}
+//
+//int main() {
+//	double arr[10];
+//	std::cout << "输入成绩：";
+//	int count = input(arr);
+//	show(arr, count);
+//	double avg = get_avg(arr, count);
+//	std::cout << "均绩为：" << avg;
+//}
+
+//Q3/下面是一个结构声明：
+//struct box {
+//	char maker[40];
+//	float height;
+//	float width;
+//	float length;
+//	float volume;
+//};
+//
+//a.编写一个函数，按值传递box结构，并显示每个成员
+//void func1(box b) {
+//	std::cout << b.maker << b.height << b.width << b.length << b.volume;
+//}
+//b.编写一个函数，传递box结构的地址，并将volume成员设置为其它三维长度的乘积
+//void func2(box* b) {
+//	b->volume = (b->height) * (b->width) * (b->length);
+//	std::cout << b.maker << b.height << b.width << b.length << b.volume;
+//}
+//c.编写一个使用这两个函数的简单程序
+//int main() {
+//	box b = { "abcd", 2.5,4.0,5.5, 1.0 };
+//	func1(b);
+//	func2(&b);
+//}
+
+//Q4.为赢得头奖，玩家需从47个号码中选取5个正确号码以及从27个号码中选取1个正确号码，两个几率的乘积即为中头奖概率。计算这种概率
+//先说结论：概率是 1 / (C(47,5) * C(27,1))，所以需要先处理C(m, n)函数
+//double comb(int m, int n) {
+//	double Fenzi = 1;
+//	double Fenmu = 1;
+//	for (int i = m; i > m - n; i--)Fenzi *= i;
+//	for (int i = n; i >= 1; i--)Fenmu *= i;
+//
+//	return Fenzi / Fenmu;
+//}
+//
+//int main() {
+//	double res;
+//	res = 1 / (comb(47, 5) * comb(27, 1));
+//
+//	std::cout << "头奖概率：" << res;
+//}
+
+//Q5.定义一个递归函数，接受一个整数参数，并返回该参数阶乘。
+//int fac(int n) {
+//	if (n == 1)return 1; //触底逻辑(后一层层返回)
+//	else return n * fac(n - 1); //触底前逻辑
+//}
+//
+//int main() {
+//	int n;
+//	std::cout << "输入整数：";
+//	std::cin >> n;
+//	std::cout<<"阶乘为："<<fac(n);
+//}
+
+//Q6.某程序使用下列函数：
+//Fill_array()将一个double数组的名称和长度作为参数。它提示用户输入double值，并将这些值存储到数组中。当数组被填满或用户输入了非数字时，输入将停止，并返回实际输入了多少个数字。
+//Show_array()将一个double数组的名称和长度作为参数，并显示该数组的内容。
+//Reverse_array()将一个double数组的名称和长度作为参数，并将存储在数组中的值的顺序反转。
+//程序将使用这些函数填充数组，然后显示数组；反转数组，然后显示数组；反转数组中除第一个和最后一个元素之外的所有元素，然后显示数组。
+//int Fill_array(double arr[], size_t length) {
+//	std::cout << "输入数组元素(double): " << std::endl;
+//	int count = 0;
+//	while (count < length) {
+//		if(!(std::cin >> arr[count]))break;
+//		count++;
+//	}
+//	return count;
+//}
+//
+//void Show_array(double arr[], size_t length) {
+//	for (int i = 0; i < length; i++) std::cout << arr[i] << " ";
+//	std::cout << std::endl;
+//}
+//
+//void Reverse_array(double arr[], size_t length) {
+//	double temp;
+//	for (int i = 0; i < length / 2; i++) {
+//		temp = arr[i];
+//		arr[i] = arr[length - 1 - i];
+//		arr[length - 1 - i] = temp;
+//	}
+//}
+//
+//int main() {
+//	double arr[10];
+//	std::cout<<"目前已输入"<<Fill_array(arr, 10)<<"个数据"<<std::endl;
+//	Show_array(arr, 10);
+//	Reverse_array(arr, 10);
+//	std::cout << "反转后：" << std::endl;
+//	Show_array(arr, 10);
+//	int temp;
+//	temp = arr[0];
+//	arr[0] = arr[9];
+//	arr[9] = temp;
+//	std::cout << "交换首尾后：" << std::endl;
+//	Show_array(arr, 10);
+//}
+
+//Q7.修改程序清单7.7中的3个数组处理函数，使之使用两个指针参数来表示区间。fill_array()函数不返回实际读取了多少个数字，而是返回一个指针，该指针指向最后被填充的位置；其它的函数可以将该指针作为第二个参数，以标识数据结尾
+//double* fill_array(double* begin, double* end) {
+//	double temp;
+//	double* p = begin;
+//
+//	while (p != end) {
+//		std::cout << "输入元素：";
+//		std::cin >> temp;
+//		*p = temp; //只有解引用才能将值存入数组!!!!
+//		p++;
+//	}
+//	return p;
+//}
+//
+//void show_array(double* begin, double* end) {
+//	std::cout << "数组元素分别为：";
+//	double* p = begin;
+//	while (p != end) {
+//		std::cout << *p << " ";
+//		p++;
+//	}
+//	std::cout << std::endl;
+//}
+//
+//double sum_of_array(double* begin, double* end) {
+//	double sum = 0;
+//	double* p = begin;
+//	while (p != end) {
+//		sum += *p;
+//		p++;
+//	}
+//	return sum;
+//}
+
+//Q8.在不使用array类的情况下完成程序清单7.15所做的工作。编写两个这样的版本：
+//a.使用const char* 数组存储表示季度名称的字符串，并使用double数组存储开支
+//b.使用const char* 数组存储表示季度名称的字符串，并使用一个结构，该结构只有一个成员——一个用于存储开支的double数组。这种设计与使用array类的基本设计类似
+
+//常用的创建存储多个字符串的数组的方式只有以下四种：
+//① char* arr[] = {"str1", "str2", "str3"...}
+//② char arr[m][n] = {...} //存储m个字符串，每个字符串长度最多为n-1，因为要每个都要包含\0结束符
+//③ std::string arr[] = {...}
+//④ vector<std::string> arr = {...} 或vector<char*> arr = {...} //记住vector创建数组是不加[]的！！！！
+
+//a
+//const char* arr[4] = { "Q1", "Q2", "Q3","Q4" };
+//double cost[4] = { 100,200,300,400 };
+//
+////b
+//struct a {
+//	double cost[4];
+//};
+//const char* arr[4] = { "Q1","Q2","Q3","Q4" };
+//a cost_data = { {100,200,300,400} };
+
+//Q9.根据程序框架补全函数 //就这样吧，魔改成了自己喜欢的格式
+//#include <string>
+//struct student {
+//	std::string fullname;
+//	std::string hobby;
+//	int ooplevel;
+//};
+////getinfo()有两参数：一个指向student结构的数组首元素的指针，以及一个int值表示数组元素数量。这个函数获取并存储学生的数据，并返回实际元素的数目。
+//int getinfo(student pa[], int n) {
+//	int count = 0;
+//	while (count < n) {
+//		std::cin.ignore();
+//		getline(std::cin, pa[count].fullname);
+//		getline(std::cin, pa[count].hobby);
+//		std::cin >> pa[count].ooplevel;
+//		count++;
+//	}
+//
+//	return count;
+//}
+//
+////display1()获取student结构作为参数并且显示其内容
+//void display1(student st) {
+//	std::cout << st.fullname << st.hobby << st.ooplevel << std::endl;
+//}
+//
+////display2()获取student结构体的地址作为参数并且显示结构体内容
+//void display2(const student* ps) {
+//	std::cout << ps->fullname << ps->hobby << ps->ooplevel << std::endl;
+//}
+//
+////display3()获取student结构体中一个数组的首元素地址以及数组元素数量作为参数并显示结构体内容
+//void display3(const student pa[], int n) {
+//	for (int i = 0; i < n; i++) {
+//		std::cout << pa[i].fullname << pa[i].hobby << pa[i].ooplevel<<std::endl;
+//	}
+//}
+//
+//int main() {
+//	std::cout << "Enter class size: ";
+//	int class_size;
+//	std::cin >> class_size;
+//	while (std::cin.get() != '\n') continue;
+//
+//	student* ptr_stu = new student[class_size];
+//	int entered = getinfo(ptr_stu, class_size);
+//	for (int i = 0; i < entered; i++) {
+//		display1(ptr_stu[i]);
+//		display2(&ptr_stu[i]);
+//	}
+//	display3(ptr_stu, entered);
+//	delete[] ptr_stu;
+//	ptr_stu = nullptr;
+//	std::cout << "Done\n";
+//	return 0;
+//}
+
+//Q10.设计double类型calculate()函数，接受两个double值和一个指向函数的指针，而被指向的函数接受两个double参数，并返回一个double值。calculate()函数返回被指向的函数的使用calculate()的两个double参数计算得到的值。
+//假设add()函数定义如下,也就是说calculate()是调用其他函数的函数
+//double add(double x, double y) {
+//	return x + y;
+//}
+//
+//double calculate(double a, double b, double(*func)(double, double)) { //形参是指向其它函数的指针的设计(死记硬背)：指向函数的返回值类型(*func)(指向函数形参类型)
+//	return func(a, b);
+//}
---
+++ Chapter7/Review/Review.cpp
@@ -0,0 +1,171 @@
+﻿//Q1.使用函数的3个步骤
+//声明、定义、调用(传参)
+//
+//Q2.创建与下面的描述匹配的函数原型
+//a.igor()没有参数，且没有返回值---void igor()
+//b.tofu()接受一个int参数，并返回一个float---float tofu(int n)
+//c.mpg()接受两个double参数，并返回一个double--double mpg(double m, double n)
+//d.summation()将long数组名和数组长度作为参数，并返回一个long值---long summation(long arr[], size_t length) //size_t常用于表示容器长度，其无符号整型特性确保不会出现负数
+//e.doctor()接受一个字符串参数(不能修改该字符串)，并返回一个double值---double doctor(const std::string& str) //当传入对象较大时(如大数组、字符串、复杂类等)使用&，且用const保证不被外部修改
+//f.ofcourse()将boss结构作为参数，不返回值---void ofcourse(struct boss b) //确保结构体boss已经定义
+//g.plot()将map结构的指针作为参数，并返回一个字符串---std::string plot(map* p)
+//
+//Q3.编写一个接受3个参数的函数：int 数组名、数组长度和一个int值，并将数组所有元素都设置为该int值
+//void func(int arr[], size_t length, int x) { //数组作为函数参数时不需要使用引用符号 &，只需要传递 int arr[] 或 int* arr，这会自动按引用传递数组。
+//	for (int i = 0; i < length; i++) arr[i] = x;
+//}
+//
+//Q4.编写一个接受3个参数的函数：指向数组区间中第一个元素的指针、指向数组区间最后一个元素后面的指针以及一个int值，并将数组中每个元素都设置为该int值。并将数组中每个元素都设置为该int值
+//void func(int* begin, int* end, int value) {
+//	while (begin != end) {
+//		*begin = value;
+//		begin++;
+//	}
+//}
+//
+//Q5.将double数组名和数组长度作为参数，并返回该数组中最大值。该函数不应修改数组的内容
+//double func(double arr[], size_t length) {
+//	if (length == 0)return 1; //数组长度为0则报错
+//	double max = arr[0]; //将max初始化为arr[0]而不是0避免出现负数数组比不了情况
+//	for (int i = 0; i < length; i++) {
+//		if (arr[i] > max) max = arr[i];
+//	}
+//	return max;
+//}
+//
+//Q6.为什么不对类型为基本类型的函数参数使用const限定符
+//因为就算不用const函数内部也改变不了原始数据
+//
+//Q7.C++程序可使用哪3种C风格字符串格式
+//字符数组：通过字符数组存储字符串，char str[] = "Hello";
+//
+//字符指针：通过字符指针指向字符串，const char* str = "Hello";
+//
+//常量字符指针：指针本身是常量，不能修改指针，但可以修改内容，char* const str = "Hello"; 
+//
+//Q8.函数原型如下：int replace(char* str, char c1, char c2); 该函数将字符串中所有c1替换成c2，并返回替换次数
+//int replace(char* str, char c1, char c2) {
+//	int count = 0;
+//	while (*str != '\0') { //这是C风格字符串，没有end()成员，所以得解引用看*str是不是\0来判断有没有到末尾
+//		if (*str == c1) *str = c2;
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//Q9.表达式*"pizza"的含义是什么？"taco"[2]呢？
+//C++中，字符串常量就是一个指向字符数组的指针。
+//所以"pizza"本质是一个指向这个字符串的指针，指向其第一个字符，("pizza" -> {'p', 'i', 'z', 'z', 'a', '\0'})，所以将其解引用后就是'p'
+//"taco"[2]是对字符数组下标为2的元素访问，即为'c'
+//
+//Q10.C++允许按值传递结构，也允许传递结构的地址。如果glitz是一个结构变量，如何按值传递它？如何传递它的地址？这两种方式有何利弊？
+//struct Glitz {
+//	int value;
+//};
+////按值传递：
+//void func(Glitz g) {
+//	g.value = 1; //按值传递时，函数接收到的是结构体的副本，也就是说函数内部对结构体的修改不会影响到原始结构体。
+//}
+//int main() {
+//	Glitz g = { 0 };
+//	func(g);
+//	cout << g.value;//由于更改的只有副本所以输出初始化给它的值，即为0
+//}
+//
+////按地址传递(传指针)
+//void func(Glitz* g) {
+//	g->value = 1;
+//}
+//int main() {
+//	Glitz g = { 0 };
+//	func(&g);//记住形参有*调用时实参要加&!!!!!!!!
+//	cout << g.value;//传地址即能改变原始数据，故输出1
+//}
+//
+////按地址传递(传引用)
+//void func(Glitz& g) {
+//	g.value = 1;
+//}
+//int main() {
+//	Glitz g = { 0 };
+//	func(g);//函数声明时若形参是&引用，则相当于直接对原始对象操作，仅需不加任何修饰地传入对象即可
+//	cout << g.value;
+//}
+//
+//综上，按值传递适用于数据较小且不需要修改原始数据的情况，保证了数据的安全性；按地址传递适用于数据较大且需要在函数中修改原始数据的情况，但需要小心副作用和指针的管理。
+//
+//Q11.函数judge()返回类型为int，它将这样一个函数的地址作为参数：将const char指针作为参数，并返回一个int值。编写judge()函数的原型
+//int judge(const char* p)
+//
+//例如：
+//int judge(const char* p) {
+//	if (p == nullptr) {
+//		return -1;  // 如果传入的指针为空，则返回 -1
+//	}
+//
+//	// 假设我们根据字符串的长度返回不同的值
+//	int length = 0;
+//	while (p[length] != '\0') {//C++特性，指针直接当数组使
+//		length++;
+//	}
+//
+//	return length;  // 返回字符串的长度
+//}
+//
+//int main() {
+//	const char* testStr = "Hello, world!";
+//	int result = judge(testStr);
+//	std::cout << "The length of the string is: " << result << std::endl;  // 输出字符串的长度
+//	return 0;
+//}
+//
+//Q12.假设有如下结构声明：
+//struct applicant {
+//	char name[30];
+//	int credit_ratings[3];
+//};
+//a.编写函数，它将applicant结构作为参数，并显示该结构的内容
+//void func(applicant a) {
+//	cout << a.name << a.credit_ratings;
+//}
+//int main() {
+//	applicant a = { a, b };
+//	func(a);
+//}
+//b,编写函数，它将applicant结构的地址作为参数，并显示该参数指向的结构的内容
+//void func(applicant* a) {
+//	cout << a->name << a->credit_ratings;
+//}
+//int main() {
+//	applicant a = { a, b };
+//	func(&a);
+//}
+//
+//Q13.假设函数f1()和f2()的原型如下：
+//void f1(applicant* a);
+//const char* f2(const applicant* a1, const applicant* a2);
+//请将p1和p2分别声明为指向f1和f2的指针；将ap声明为一个数组，它包含5个类型于p1相同的指针；将pa声明为一个指针，它指向的数组包含10个类型与p2相同的指针。使用typedef辅助完成任务。
+//
+//// 使用 typedef 定义函数指针类型
+//typedef void (*f1_ptr)(applicant*);
+//typedef const char* (*f2_ptr)(const applicant*, const applicant*);
+//
+//int main() {
+//	// 声明指向 f1 和 f2 的函数指针
+//	f1_ptr p1;
+//	f2_ptr p2;
+//
+//	// 初始化指向 f1 和 f2 函数的指针
+//	p1 = f1;
+//	p2 = f2;
+//
+//	// 声明指针数组 ap 和 pa
+//	f1_ptr ap[5];  // ap 是一个包含 5 个指向 f1 函数的指针的数组
+//	f2_ptr(*pa)[10];  // pa 是一个指向包含 10 个指向 f2 函数的指针的数组的指针
+//
+//	// 使用函数指针进行调用
+//	applicant a = { "John Doe", {800, 700, 900} };
+//	p1(&a);  // 调用 f1
+//
+//	cout << p2(&a, &a) << endl;  // 调用 f2
